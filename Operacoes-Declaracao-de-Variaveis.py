e41be18466f297062647ba6e6c14afbd0db4f9b9
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-03-28 20:10:23 -0300

    Calculadora-de-Somar-Inteiros

--- Operacoes-Declaracao-de-Variaveis.py
+++ Operacoes-Declaracao-de-Variaveis.py
@@ -9,3 +9,15 @@ VALOR = float(VALOR)
 CONVERSAO = VALOR * COTACAO
 print("O valor total em moeda local é: ", CONVERSAO)
 
+
+# Calculadora de Somar Inteiros
+# O sistema pede a entrada do primeiro valor, do tipo inteiro
+# O sistema pede a entrada do segundo valor, do tipo inteiro
+# O sistema imprime a soma dos dois valores
+VAL1 = input("Informe o primeiro valor: ")
+VAL2 = input("Informe o segundo valor: ")
+VAL1 = int(VAL1)
+VAL2 = int(VAL2)
+VAL3 = VAL1 + VAL2
+print("Total dos valores: ", VAL3)
+
