dccea38896208c69c912c76ff8273e9283d4d68c
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-03-29 21:22:48 -0300

    Operacoes

---
+++ Operacoes-Declaracao-de-Variavel/Operacoes.py
@@ -0,0 +1,11 @@
+# O Sistema deve ler um número inteiro
+# Deve então imprimir:
+# O antecessor deste numero
+# O sucessor deste número
+# A potência deste número
+
+var1 = input("Informe um número inteiro: ")
+var1 = int(var1)
+print("Antecessor: ", var1 -1)
+print("Sucessor: " var1 +1)
+print("Potência: " var1 * var1)
