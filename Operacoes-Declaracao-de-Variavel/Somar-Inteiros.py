9b82472c501f16d9034a7c01d8dbe2fe79c193b3
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-03-29 21:14:22 -0300

    Inversao

---
+++ Operacoes-Declaracao-de-Variavel/Somar-Inteiros.py
@@ -0,0 +1,15 @@
+# Calculadora de Somar Inteiros
+# O sistema pede a entrada do primeiro valor, do tipo inteiro
+# O sistema pede a entrada do segundo valor, do tipo inteiro
+# O sistema imprime a soma dos dois valores
+VAL1 = input("Informe o primeiro valor: ")
+VAL2 = input("Informe o segundo valor: ")
+VAL1 = int(VAL1)
+VAL2 = int(VAL2)
+VAL3 = VAL1 + VAL2
+print("Total dos valores: ", VAL3)
+
+# Versão dois:
+VALN = input("Informe o primeiro valor: ")
+VALM = input("Informe o segundo valor: ")
+print = ("Total dos valores: ", int(VALN) + int(VALM))
