771ee611a444da5ffbf8a8139f5927d05e6c8192
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-04-01 15:34:23 -0300

    Percentuais

---
+++ Operacoes-Declaracao-de-Variavel/Percentuais.py
@@ -0,0 +1,9 @@
+# O programa deve ler a quantidade total de alunos de uma escola
+# O programa deve ler a quantidade de alunos do sexo masculino
+# O programa deve imprimir o percentual de alunos do sexo masculino e feminino
+total = input("Informe o total de alunos: ")
+total = int(total)
+masculino = input("Informe o total de alunos do sexo masuclino: ")
+masculino = int(masculino)
+print("Percentual de alunos do sexo masculino: ", masculino * 100 / total)
+print("Percentual de alunos do sexo feminino: ", (total - masculino * 100 / total)) 
