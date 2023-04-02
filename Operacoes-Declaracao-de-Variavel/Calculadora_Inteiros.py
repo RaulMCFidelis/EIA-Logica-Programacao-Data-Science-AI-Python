a7eeea4a056aae3185556c2b92afdcc2c0a94b1a
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-04-02 20:02:56 -0300

    Calculadora_Inteiros

---
+++ Operacoes-Declaracao-de-Variavel/Calculadora_Inteiros.py
@@ -0,0 +1,10 @@
+# O sistema pede a entrada do primeiro valor, do tipo inteiro
+# O sistema pede a entrada do segundo valor, do tipo inteiro
+# O sistema imprime:
+    # A Soma dos dois valores
+    # O Produto dos valores
+primeiroV =  int(input("Informe o primeiro valor: "))
+segundoV =  int(input("Informe o segundo valor: "))
+soma = primeiroV + segundoV
+produto = primeiroV * segundoV
+print("A soma dos valores é : ",soma, " e o produtos dos valores é ", produto)
