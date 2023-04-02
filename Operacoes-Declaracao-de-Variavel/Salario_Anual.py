0672a1b190a8b6203309dcc3db361a6157c4a2c7
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-04-02 20:09:46 -0300

    Salario_Anual

---
+++ Operacoes-Declaracao-de-Variavel/Salario_Anual.py
@@ -0,0 +1,5 @@
+# O sistema deve ler o salário de um funcionário
+# O sistema deve calcular quanto o funcionário deve receber por mês
+salario =  float(input("Informe o salário: "))
+salariomensal = salario / 12
+print("O salário mensal é igual a  ", salariomensal)
