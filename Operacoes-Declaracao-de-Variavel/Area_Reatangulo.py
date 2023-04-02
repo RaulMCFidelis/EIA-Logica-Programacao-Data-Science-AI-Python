323e4248867b0f683ec6d0a641f17769aa60ce57
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-04-02 18:37:18 -0300

    Area-Retangulo

---
+++ Operacoes-Declaracao-de-Variavel/Area_Reatangulo.py
@@ -0,0 +1,6 @@
+# O programa deve fazer leitura da largura e altura de um retângulo
+# O sistema devera imprimir a área do retângulo
+largura =  float(input("Informe a largura do retângulo: "))
+altura =  float(input("Informe a altura do retângulo: "))
+area = largura * altura
+print("A área do retângulo é de : ",area)
