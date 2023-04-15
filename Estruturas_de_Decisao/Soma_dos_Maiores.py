fbd8902784b60eccad0f4cd9f3d2e455c5317fb2
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-04-15 19:03:46 -0300

    Soma dos Maiores

---
+++ Estruturas_de_Decisao/Soma_dos_Maiores.py
@@ -0,0 +1,9 @@
+# Soma dos Maiores
+# Faça a leitura de 2 valores inteiros
+# Imprima os valores em ordem crescente
+val1 = int(input("Informe o primeiro valor: "))
+val2 = int(input("Informe o segundo valor: "))
+if val1 < val2 :
+    print(val1, " ", val2)
+else:
+    print(val2, " ", val1)
