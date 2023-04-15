da71b94ada5d35858f679bb75dbc0ee3fb7482cc
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-04-15 18:57:29 -0300

    Valor Positivo ou Negativo ou Zero

---
+++ Estruturas_de_Decisao/Positivo_ou_Negativo.py
@@ -0,0 +1,10 @@
+# Positivo ou Negativo
+# O sistema deve ler um valor inteiro
+# Imprimir se o valor for pogitivo e negativo se for negativo e neutro se for zero
+val = int(input("Informe um valor: "))
+if val<0:
+    print("Número Negativo")
+elif val>0:
+     print("Número Positivo")
+else:
+    print("Número Neutro")
