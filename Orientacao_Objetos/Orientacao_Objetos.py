beec6f83bfee69a043064ee7de6885161851e9bf
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-04-20 20:42:08 -0300

    Orientação a Objetos

---
+++ Orientacao_Objetos/Orientacao_Objetos.py
@@ -0,0 +1,13 @@
+# Orientação a Objetos
+class aluno:
+    def __init__(self, matricula,nome):
+        self.matricula = matricula
+        self.nome = nome
+    def matricular(self):
+        print("Aluno ", self.nome, " matriculado")
+
+
+jose = aluno("123","José da Silva")
+print(jose.matricula)
+print(jose.nome)
+jose.matricular()
