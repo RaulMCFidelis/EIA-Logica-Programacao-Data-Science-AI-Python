c26d08750da3d6ed953a3f1440cbbcb398a71c0a
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-04-22 18:00:37 -0300

    Maior e Menor Número de Duas Formas Possíveis

---
+++ Problemas_Extras/Maior_e_Menor.py
@@ -0,0 +1,22 @@
+# Maior e Menor
+# Escreva um programa que leia 10 números inteiros
+# Imprima o maior e o menor número
+# Deve usar listas
+lista = list(range(1,11))
+print(lista)
+
+#entrada de dados
+for n in range(0, 10):
+    print("Informe o número da posição ", n+1)
+    lista[n] = int(input())
+
+
+#forma 1, ordenar e exibir primeiro e ultimo elemento
+lista2 = sorted(lista)
+print("O maior valor é: " ,  lista2[9])
+print("O Menor valor é: ", lista2[0])
+
+
+#forma dois, max e min  
+print("O maior valor é: " ,  max(lista))
+print("O Menor valor é: ", min(lista))
