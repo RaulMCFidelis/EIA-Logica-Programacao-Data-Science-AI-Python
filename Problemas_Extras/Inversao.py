9d4d79c0f2c21865484ca8e648ac2283436c1098
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-04-28 15:48:23 -0300

    Inversão e Raiz Quadrada

---
+++ Problemas_Extras/Inversao.py
@@ -0,0 +1,21 @@
+# Inversão
+# O programa deve solicitar uma lista com 5 números reais
+# Para cada número, deve informar:
+    # A raiz quadrada
+    # Se a raiz quadrada do número é um valor inteiro ou real(com casas decimais)
+# Deve usar listas
+from math import sqrt
+
+lista = list(range(0,5))
+
+
+for n in range(0, 5):
+    print("Informe o número da posição ", n+1, " da primeira lista")
+    lista[n] = float(input())
+
+for n in range(0, 5):
+    print("Raiz quadrada: ", sqrt(lista[n]))
+    if sqrt(lista[n]) % 1 == 0 :
+        print("Raiz quadrada é um valor inteiro")
+    else:
+        print("Raiz quadrada não é um valor inteiro")
